2937516a0da99117afb1e3b2d2bc8527a27c0f8c
n-ndini7 <hp@RJs-PC>
2020-09-28 01:18:02 +0530

    [R.Nandini] added Master - Welcome to address book program

---
+++ src/com/capgemini/AddressBook.java
@@ -0,0 +1,9 @@
+package com.capgemini;
+
+
+public class AddressBook {
+	//Master branch - welcome message
+		public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program!!");
+	}
+}
